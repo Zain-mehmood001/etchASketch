23e2f26c9ebfb856e2375890fcc1cf4419f4eee1
zain <zaino.zm7@gmail.com>
2022-08-06 17:44:11 +0500

    Complete the project! Looks Cool

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
-const container = document.querySelector(".grid-container"); // select the container
-for(let i = 0; i < 16*16; i++) // creats 256 divs
-{
-    const block = document.createElement('div');
-    block.style.width = '5.5%';// 5.5%
-    block.style.height = '0';
-    block.style.paddingBottom = '5.5%';     // 5.5%
-    block.style.margin = '1.5px';
-    block.textContent = i + 1;
-    block.classList.add('cell');
-    block.style.backgroundColor = 'gray';
-    container.appendChild(block);
+let noOfCells = 16; // default size of grid
+
+function makegrid(){
+
+    const container = document.querySelector(".grid-container"); // select the container
+
+    const cellHeight = (window.innerHeight / noOfCells) - 1; // gets the windows max height which in my case is 670px
+                    // divides it on the number of cells we wanna have and minus 1px because of the gap in between
+    const cellWidth = cellHeight;
+
+    for(let i = 0; i < noOfCells*noOfCells; i++) // creats 256 divs
+    {
+        const block = document.createElement('div');
+        block.style.width = cellWidth + "px";// 5.5%
+        block.style.height = cellHeight + "px";
+        block.classList.add('cell');
+        block.style.backgroundColor = 'black';
+        container.appendChild(block);
+    }
+
+    const cells = document.querySelectorAll('.grid-container .cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter',
+        function changeColor(){
+            cell.style.transition = '0s';
+            /*
+            let x = Math.floor(Math.random()*255);
+            let y =  Math.floor(Math.random()*255);
+            let z = Math.floor(Math.random()*255);
+            cell.style.backgroundColor = 
+            "#"+
+            (x).toString(16)+
+            (y).toString(16)+
+            (z).toString(16); // complete random rgb*/
+            cell.style.backgroundColor = 'cyan';
+            cell.style.transform = 'scale(1.5, 1.5)';
+        });
+    });
+    
+    cells.forEach(cell => {
+        cell.addEventListener('mouseleave',
+        function changeColor(){
+            cell.style.transform = 'scale(1, 1)';
+            cell.style.transition = '10s';
+            cell.style.backgroundColor = "black";
+        });
+    });
 }
 
-const cells = document.querySelectorAll('.grid-container .cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseenter',
-    function changeColor(){
-        //cell.style.transition = '0s';
-        cell.style.backgroundColor = "lightblue";
-    });
-});
-/*
-cells.forEach(cell => {
-    cell.addEventListener('mouseleave',
-    function changeColor(){
-        //cell.style.transition = '1.00s';
-        cell.style.backgroundColor = "gray";
-    });
-});*/
+makegrid(); // make a grid initially
+
+function removeGrid()
+{
+    let grid = document.querySelector('.grid-container'); // select the grid wala container
+    while(grid.firstChild)
+    {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function newgrid()
+{
+    // remove existing grid
+    removeGrid();
+
+    // make new grid
+    makegrid();
+}
+
+function getcells()
+{
+    noOfCells = parseInt(prompt("Enter number of squares per side: "));
+    console.log(noOfCells);
+    if(noOfCells > 100 || noOfCells < 1 || isNaN(noOfCells))
+    {
+        noOfCells = 16;
+        alert("Enter a valid number! 1 - 100");
+    }
+    newgrid();
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', getcells);
